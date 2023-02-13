0e6e81bbc52c111cc0090aa6c5c770c20904bd02
Ha The Tung <tunght13488@gmail.com>
2023-02-13 22:44:30 +0800

    doc: correct doc

--- handlers/login.go
+++ handlers/login.go
@@ -8,7 +8,7 @@ import (
 	"time"
 )
 
-// TwtCustomClaims are custom claims extending default ones.
+// JwtCustomClaims are custom claims extending default ones.
 // See https://github.com/golang-jwt/jwt for more examples
 type JwtCustomClaims struct {
 	Name  string `json:"name"`
